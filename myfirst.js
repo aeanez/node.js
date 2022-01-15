840e64e247c73c2bc12b916237b552f0e51f6747
Andrés Añez <aeanez@gmail.com>
2022-01-15 14:37:28 -0500

    Printing the Query string

--- myfirst.js
+++ myfirst.js
@@ -4,6 +4,7 @@ var dt = require('./modules/dateModule');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!<br>');
-  res.write('The current date is: ' + dt.myDateTime());
+  res.write('The current date is: ' + dt.myDateTime()+ "<br>");
+  res.write('The requested URL is: ' + req.url + "<br>");
   res.end();
 }).listen(8081); 
