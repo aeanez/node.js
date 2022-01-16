b7592ff3e088838d3059fef5f2e4a452dbd132d6
Andrés Añez <aeanez@gmail.com>
2022-01-16 12:35:20 -0500

    Loading header + content + footer using promises

--- myfirst.js
+++ myfirst.js
@@ -4,32 +4,35 @@ var url = require('url');
 var fs = require('fs'); 
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    var html = "";
+    loadView('header').then((data) => {
 
-    fs.promises.readFile('./views/header.html', "UTF-8", function (err, data) {
+        html += data;
+        var q = url.parse(req.url, true)
+        
+        if(q.pathname != "/" && q.pathname != undefined){
+            return loadView(q.pathname);
+        }
+        return new Promise((res, rej) => {
+            res('404 Page not found');
+        });
+    })
+    .then((data) => {
+        html += data;
+        return loadView('footer');
+    }).then((data) => {
+        html += data;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        res.end();
+
+    });
+}).listen(8081); 
+
+
+async function loadView(viewName) {
+    return Promise.resolve(fs.promises.readFile('./views/' + viewName + '.html', {encoding: 'UTF-8'}, (err, data) => {
         if (err) throw err;
         return data;
-    }).then( (data) => {
-        res. write(data);
-        res.write('Hello World!<br>');
-        res.write('The current date is: ' + dt.myDateTime()+ "<br>");
-        res.write('The Query String is: ' + req.url + "<br>");
-    
-        var query = url.parse(req.url, true).query
-        var txt = query.year + " " + query.month
-    
-        res.write('Spliting the Query string: ' + txt + "<br>");
-        res.write('<a href="http://localhost:8081/?year=2017&month=July">Click me</a><br>');
-
-    }).then(() => {
-        fs.promises.readFile('./views/footer.html', (err, data) => {
-            if (err) throw err;
-            res.write(data);
-        }).then(() => {
-            res.end();
-        });
-    });
-
-
-    
-}).listen(8081); 
+    }));
+}
